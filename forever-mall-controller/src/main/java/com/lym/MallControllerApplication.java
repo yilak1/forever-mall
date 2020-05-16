78294339de2b07787b4be053552b7d2d73ce0fd1
yilak1 <ldsscholar@163.com>
2020-05-16 12:41:00 +0800

    引入注册模块版本

--- forever-mall-controller/src/main/java/com/lym/MallControllerApplication.java
+++ forever-mall-controller/src/main/java/com/lym/MallControllerApplication.java
@@ -2,10 +2,12 @@ package com.lym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.lym.mapper")
+@ComponentScan(basePackages = {"com.lym", "org.n3r.idworker"})
 public class MallControllerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallControllerApplication.class, args);
