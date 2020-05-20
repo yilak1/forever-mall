f3aa2aef6bbfa842c5b27f592dde32d5cce704f3
chengyiming <2063439269@qq.com>
2020-05-20 23:43:32 +0800

    个人中心

---
+++ forever-mall-service/src/main/java/com/lym/service/center/CenterUserService.java
@@ -0,0 +1,23 @@
+package com.lym.service.center;
+
+import com.lym.pojo.Users;
+import com.lym.pojo.bo.center.CenterUserBO;
+
+public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     * @param userId
+     * @return
+     */
+    public Users queryUserInfo(String userId);
+
+    /**
+     * 修改用户信息
+     * @param userId
+     * @param centerUserBO
+     */
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+
+}
---
+++ forever-mall-service/src/main/java/com/lym/service/impl/center/CenterUserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.lym.service.impl.center;
+
+import com.lym.mapper.UsersMapper;
+import com.lym.pojo.Users;
+import com.lym.pojo.bo.center.CenterUserBO;
+import com.lym.service.center.CenterUserService;
+import com.lym.mapper.UsersMapper;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+public class CenterUserServiceImpl implements CenterUserService {
+
+    @Autowired
+    public UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserInfo(String userId) {
+        Users user = usersMapper.selectByPrimaryKey(userId);
+        user.setPassword(null);
+        return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users updateUserInfo(String userId, CenterUserBO centerUserBO) {
+
+        Users updateUser = new Users();
+        BeanUtils.copyProperties(centerUserBO, updateUser);
+        updateUser.setId(userId);
+        updateUser.setUpdatedTime(new Date());
+
+        usersMapper.updateByPrimaryKeySelective(updateUser);
+
+        return queryUserInfo(userId);
+    }
+
+
+}
